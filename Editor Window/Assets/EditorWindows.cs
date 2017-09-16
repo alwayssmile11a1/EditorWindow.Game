5b628e24fd6d0f93af2083c3772d3026c1ffb5ba
Trương Ngọc Sơn <alwayssmile11a1@outlook.com>
2017-09-16 22:05:58 +0700

    BasicWindow

---
+++ Editor Window/Assets/EditorWindows.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorWindows : EditorWindow {
+
+    string myString = "Hello, World";
+
+    Color color;
+
+    [MenuItem("Window/MyWindow")]
+    public static void ShowWindow()
+    {
+        GetWindow<EditorWindows>("MyWindow");
+    }
+
+	void OnGUI()
+    {
+        //Window code
+        GUILayout.Label("This is my Window Editor", EditorStyles.boldLabel);
+
+        myString = EditorGUILayout.TextField("Name", myString);
+
+        color = EditorGUILayout.ColorField("Color", color);
+
+        if(GUILayout.Button("Press me"))
+        {
+            foreach(GameObject obj in Selection.gameObjects)
+            {
+                Renderer renderer = obj.GetComponent<Renderer>();
+                if(renderer!=null)
+                {
+                    renderer.sharedMaterial.color = color;
+                }
+            }
+        }
+
+    }
+}
